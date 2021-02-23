d3217ca115e320af569cae8342638e43f35faa6a
jonathan <jonathanarmored@yahoo.co.id>
2021-02-23 14:34:06 +0700

    membuat file controller

--- koneksi.js
+++ koneksi.js
@@ -7,7 +7,7 @@ const conn = mysql.createConnection({
     database : 'dbrestapi'
 });
 
-conn connect((err) => {
+conn.connect((err) => {
     if (err) throw err;
     console.log('Mysql Connected');
 });
